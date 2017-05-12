5bc1f76c41767526268162a2dc964ae15522debc
Andre Kohn <andre@kohn.io>
2017-05-12 17:00:20 +0200

    Use better git log

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -11,28 +11,10 @@ else
     colorflag="-G"
 fi
 
-alias l="ls -lah ${colorflag}"
-alias la="ls -AF ${colorflag}"
 alias ll="ls -lFh ${colorflag}"
-alias lld="ls -l | grep ^d"
-alias rmf="rm -rf"
 
 alias grep='grep --color=auto'
 alias df='df -h'
 alias du='du -h -c'
 
-alias flushdns="dscacheutil -flushcache"
-
-alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
-alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
-
-alias trimcopy="tr -d '\n' | pbcopy"
-
-alias fs="stat -f \"%z bytes\""
-
-for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
-    alias "$method"="lwp-request -m '$method'"
-done
-
-alias stfu="osascript -e 'set volume output muted true'"
-alias pumpitup="osascript -e 'set volume 10'"
+alias lg="git log --graph --abbrev-commit --decorate --format=format:'%C(normal)%h%C(reset) %C(dim white)- %an (%ar) -%C(reset) %C(normal)%s%C(reset) %C(bold normal)%d%C(reset)' --all"
