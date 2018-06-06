17333c769ba63a11206da4382a99ced65cb21ab5
Andre Kohn <andre@kohn.io>
2018-06-06 20:26:32 +0200

    Throw out bash aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -11,12 +11,8 @@ else
     colorflag="-G"
 fi
 
-alias ll="ls -lFh ${colorflag}"
-
 alias grep='grep --color=auto'
 alias df='df -h'
 alias du='du -h -c'
 
 alias lg="git log --graph --abbrev-commit --decorate --format=format:'%C(normal)%h%C(reset) %C(dim white)- %an (%ar) -%C(reset) %C(normal)%s%C(reset) %C(bold normal)%d%C(reset)' --all"
-
-alias topo="lstopo"
