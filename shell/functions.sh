a0857087225d022fa6c7594c8fe7ebaa27f0743b
Andre Kohn <kohn.a@outlook.com>
2020-02-01 18:42:10 +0100

    Tweak caravan image

--- shell/functions.sh
+++ shell/functions.sh
@@ -10,30 +10,6 @@ function colours() {
     done
 }
 
-# Find shorthand
-function f() {
-    find . -name "$1"
-}
-
-# Take this repo and copy it to somewhere else minus the .git stuff.
-function gitexport(){
-    mkdir -p "$1"
-    git archive master | tar -x -C "$1"
-}
-
-# Get gzipped size
-function gz() {
-    echo "orig size    (bytes): "
-    cat "$1" | wc -c
-    echo "gzipped size (bytes): "
-    gzip -c "$1" | wc -c
-}
-
-# All the dig info
-function digga() {
-    dig +nocmd "$1" any +multiline +noall +answer
-}
-
 # Extract archives - use: extract <file>
 # Credits to http://dotfiles.org/~pseup/.bashrc
 function extract() {
@@ -58,23 +34,6 @@ function extract() {
     fi
 }
 
-# Reset date of last commit
-function git-fix-date() {
-    git commit --amend --reset-author --no-edit
-}
-
-# Docker cleanup
-function docker-cleanup() {
-   docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
-   docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
-}
-
-# Docker reset
-function docker-reset() {
-   docker rm -vf $(docker ps -a -q) 2>/dev/null
-   docker rmi -f $(docker images -q) 2>/dev/null
-}
-
 # Find fat
 function find-fat-things() {
     du -ahx / | sort -rh | head -100
@@ -83,29 +42,6 @@ function find-fat-things() {
 # Dump clang record layouts
 function dump-record-layout() {
     clang++ -std=c++17 -Xclang -fdump-record-layouts $1
-}
-
-# Clean old kernels
-function clean-kernels () {
-    # Patterns
-    local current_kernel="$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')"
-    local kernel_pkg="linux-(image|headers|ubuntu-modules|restricted-modules)"
-    local meta_pkg="${kernel_pkg}-(generic|i386|server|common|rt|xen|ec2)"
-
-    # Gather
-    local to_keep="$(dpkg -l | egrep ${kernel_pkg} | egrep "${current_kernel}|${meta_pkg}" | awk '{print $2}')"
-    local to_remove="$(dpkg -l | egrep ${kernel_pkg} | egrep -v "${current_kernel}|${meta_pkg}" | awk '{print $2}')"
-    printf "KEEP:\n%s\n\nREMOVE:\n%s\n" "${to_keep}" "${to_remove}"
-
-    # Ask if sane
-    printf "\nAre you sure? ([yY]|[nN])\n"
-    read sane
-    if [[ ! ${sane} =~ ^[Yy]$ ]]; then
-        return 1;
-    fi
-
-    # Purge kernels
-    sudo apt-get purge $(echo ${to_remove})
 }
 
 # Find llvm component
@@ -125,22 +61,3 @@ function llvm-grep() {
         fi
     done
 }
-
-# Remove something from the path
-function rpath() {
-    for path in "$@";do
-        PATH="$(echo "$PATH" |sed -e "s#\(^\|:\)$(echo "$path" |sed -e 's/[^^]/[&]/g' -e 's/\^/\\^/g')\(:\|/\{0,1\}$\)#\1\2#" -e 's#:\+#:#g' -e 's#^:\|:$##g')"
-    done
-    echo "$PATH"
-}
-
-# Disable ICECC
-icecc_disable() {
-    export PATH="$(rpath /usr/lib/icecc/bin)"
-}
-
-# Enable ICECC
-icecc_enable() {
-    export PATH="/usr/lib/icecc/bin:$PATH"
-}
-
