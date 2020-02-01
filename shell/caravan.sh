d45b1151733c0c68611958252f97616c3b2c7fd1
Andre Kohn <kohn.a@outlook.com>
2020-02-01 19:51:33 +0100

    Work on caravan

--- shell/caravan.sh
+++ shell/caravan.sh
@@ -1,6 +1,7 @@
 function setup_caravan() {
     if [ -z "$1" ]; then
         echo "Usage: setup_caravan <name>"
+        return
     fi
     docker run -d \
         --name "$1" \
@@ -12,14 +13,16 @@ function setup_caravan() {
 function enter_caravan() {
     if [ -z "$1" ]; then
         echo "Usage: enter_caravan <name>"
+        return
     fi
     CARAVAN_PORT=$(docker inspect --format '{{ (index (index .NetworkSettings.Ports "22/tcp") 0).HostPort }}' "$1")
-    ssh -i ~/.ssh/caravan/caravan -p ${CARAVAN_PORT} 0.0.0.0
+    ssh -i ~/.ssh/caravan/caravan -p ${CARAVAN_PORT} localhost
 }
 
 function destroy_caravan() {
     if [ -z "$1" ]; then
         echo "Usage: destroy_caravan <name>"
+        return
     fi
     docker stop "$1" || true
     docker rm -vf "$1" || true
