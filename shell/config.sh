974be4fefd9263ceaa5809437c4ffcfa02d0f0e6
Andre Kohn <kohn.a@outlook.com>
2020-05-12 13:45:52 +0200

    Init pyenv

--- shell/config.sh
+++ shell/config.sh
@@ -45,6 +45,12 @@ export PATH=$GEM_HOME/bin:$PATH
 export PATH=$PATH:~/.fzf/bin
 export PATH=$PYENV_ROOT/bin:$PATH
 
+# Load pyenv
+if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+fi
+
+# Load nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
