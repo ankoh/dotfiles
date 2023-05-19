30766e704939ea6be6217ac2a232fce0b8c34b3a
Andre Kohn <kohn.a@outlook.com>
2023-05-19 16:15:10 +0200

    Use 1PW ssh agent in WSL

---
+++ other/wslssh.sh
@@ -0,0 +1,21 @@
+mkdir -p ~/.1password
+
+# Taken from here: https://dev.to/d4vsanchez/use-1password-ssh-agent-in-wsl-2j6m
+
+# Configure ssh forwarding
+export SSH_AUTH_SOCK=~/.1password/agent.sock
+
+# Need `ps -ww` to get non-truncated command for matching.
+# Use square brackets to generate a regex match for the process we want but that doesn't match the grep command running it!
+ALREADY_RUNNING=$(ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $?)
+if [[ $ALREADY_RUNNING != "0" ]]; then
+    if [[ -S $SSH_AUTH_SOCK ]]; then
+        # Not expecting the socket to exist as the forwarding command isn't running (http://www.tldp.org/LDP/abs/html/fto.html)
+        echo "removing previous socket..."
+        rm $SSH_AUTH_SOCK
+    fi
+    echo "Starting SSH-Agent relay..."
+    # setsid to force new session to keep running
+    # Set socat to listen on $SSH_AUTH_SOCK and forward to npiperelay which then forwards to openssh-ssh-agent on windows
+    (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+fi
