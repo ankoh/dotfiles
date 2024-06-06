b04ed10a213efc31a6c1ba882e7c4334bf980c79
André Kohn <kohn.a@outlook.com>
2024-06-06 09:35:25 +0200

    Still need luasnips

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -121,11 +121,50 @@ return { {
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {},
 }, {
+    -- load luasnips + cmp related in insert mode only
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
-    dependencies = {
-        { "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path" }
+    dependencies = { {
+        -- snippet plugin
+        "L3MON4D3/LuaSnip",
+        dependencies = "rafamadriz/friendly-snippets",
+        opts = {
+            history = true,
+            updateevents = "TextChanged,TextChangedI"
+        },
+        config = function(_, opts)
+            require("luasnip").config.set_config(opts)
+
+            -- vscode format
+            require("luasnip.loaders.from_vscode").lazy_load()
+            require("luasnip.loaders.from_vscode").lazy_load {
+                paths = vim.g.vscode_snippets_path or ""
+            }
+
+            -- snipmate format
+            require("luasnip.loaders.from_snipmate").load()
+            require("luasnip.loaders.from_snipmate").lazy_load {
+                paths = vim.g.snipmate_snippets_path or ""
+            }
+
+            -- lua format
+            require("luasnip.loaders.from_lua").load()
+            require("luasnip.loaders.from_lua").lazy_load {
+                paths = vim.g.lua_snippets_path or ""
+            }
+
+            vim.api.nvim_create_autocmd("InsertLeave", {
+                callback = function()
+                    if require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()] and
+                        not require("luasnip").session.jump_active then
+                        require("luasnip").unlink_current()
+                    end
+                end
+            })
+        end
     },
+        { "saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path" } }, -- cmp sources plugins
     opts = function()
         local cmp = require "cmp"
 
@@ -150,7 +189,11 @@ return { {
                 }
             },
 
-            snippet = {},
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end
+            },
 
             mapping = {
                 ["<C-p>"] = cmp.mapping.select_prev_item(),
@@ -166,6 +209,9 @@ return { {
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
+                    elseif require("luasnip").expand_or_jumpable() then
+                        vim.fn.feedkeys(
+                            vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
                     else
                         fallback()
                     end
@@ -173,6 +219,9 @@ return { {
                 ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
+                    elseif require("luasnip").jumpable(-1) then
+                        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true),
+                            "")
                     else
                         fallback()
                     end
@@ -180,6 +229,8 @@ return { {
             },
             sources = { {
                 name = "nvim_lsp"
+            }, {
+                name = "luasnip"
             }, {
                 name = "buffer",
                 option = {
@@ -205,5 +256,4 @@ return { {
     config = function(_, opts)
         require("cmp").setup(opts)
     end
-}
-}
+} }
