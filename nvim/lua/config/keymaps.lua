75474e774a1ad55866be2dd0360f69ce6c08d7f1
André Kohn <kohn.a@outlook.com>
2024-06-17 10:23:43 +0200

    Add ft keymapping for live_grep

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -29,6 +29,7 @@ vim.keymap.set("n", "<leader>w>", ":exe \"vertical resize \" . (winwidth(0) * 8/
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>ft", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
