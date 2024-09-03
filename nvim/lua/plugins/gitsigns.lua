bb1835bdcf4e2727805f1df6514a012aa2905eed
André Kohn <kohn.a@outlook.com>
2024-09-03 19:24:11 +0200

    Add gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -4,47 +4,9 @@ return { {
     -- Git integration for buffers
     "lewis6991/gitsigns.nvim",
     opts = {
-        signs = {
-            add = {
-                hl = "GitSignsAdd",
-                text = "│",
-                numhl = "GitSignsAddNr",
-                linehl = "GitSignsAddLn"
-            },
-            change = {
-                hl = "GitSignsChange",
-                text = "│",
-                numhl = "GitSignsChangeNr",
-                linehl = "GitSignsChangeLn"
-            },
-            delete = {
-                hl = "GitSignsDelete",
-                text = "_",
-                numhl = "GitSignsDeleteNr",
-                linehl = "GitSignsDeleteLn"
-            },
-            topdelete = {
-                hl = "GitSignsDelete",
-                text = "‾",
-                numhl = "GitSignsDeleteNr",
-                linehl = "GitSignsDeleteLn"
-            },
-            changedelete = {
-                hl = "GitSignsChange",
-                text = "~",
-                numhl = "GitSignsChangeNr",
-                linehl = "GitSignsChangeLn"
-            },
-            untracked = {
-                hl = "GitSignsAdd",
-                text = "┆",
-                numhl = "GitSignsAddNr",
-                linehl = "GitSignsAddLn"
-            }
-        },
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-        numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-        linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+        numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+        linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
         word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir = {
             interval = 1000,
@@ -58,9 +20,6 @@ return { {
             delay = 1000,
             ignore_whitespace = false
         },
-        current_line_blame_formatter_opts = {
-            relative_time = false
-        },
         sign_priority = 6,
         update_debounce = 100,
         status_formatter = nil, -- Use default
@@ -72,9 +31,6 @@ return { {
             relative = "cursor",
             row = 0,
             col = 1
-        },
-        yadm = {
-            enable = false
         }
     },
     config = function(_, opts)
